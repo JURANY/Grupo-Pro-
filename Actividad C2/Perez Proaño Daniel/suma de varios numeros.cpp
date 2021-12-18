4cf8f5eada479ab6bab0a68733c6ecb774956a1f
Daniel Perez Proaño <92699808+Danielp585@users.noreply.github.com>
2021-12-18 13:12:39 -0500

    agregue los comentarios
    en las lineas 1,7,10,12,13,16

--- Actividad C2/Perez Proaño Daniel/suma de varios numeros.cpp
+++ Actividad C2/Perez Proaño Daniel/suma de varios numeros.cpp
@@ -1,18 +1,19 @@
+//ejercicio elaborado por DANIEL PEREZ PROAÑO
 #include<iostream>
 using namespace std;
 int main(){
 	int c=0,v;
 	float a=0,l;
-	cout<<"ingrese cuantos numeros va a sumar:"; 
+	cout<<"ingrese cuantos numeros va a sumar:"; //aqui se ingresa el numero limite para la operacion matematica
 	cin>>l;
 	do{
-		cout<<"ingrese el numero:"; 
+		cout<<"ingrese el numero:";//aqui ingresa los numeros a ser sumandos 
 		cin>>v;
-		c=c+1;
-		a=a+v;
+		c=c+1;//contador 
+		a=a+v;//acumulador mas la variable
 		
 	}while (c<l);
-	cout<<"La suma de los elementos fue:"<<a<<endl;
+	cout<<"La suma de los elementos fue:"<<a<<endl;//resultado de la suma total
 	
 	
 	
