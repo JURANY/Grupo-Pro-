11e18369ca12d84d61ec936667e5da5cadc6078d
Daniel Perez Proaño <92699808+Danielp585@users.noreply.github.com>
2021-12-18 13:07:26 -0500

    modifique los comentarios

--- Actividad C2/Perez Proaño Daniel/Suma de dos numeros.cpp
+++ Actividad C2/Perez Proaño Daniel/Suma de dos numeros.cpp
@@ -1,13 +1,14 @@
+//Ejercicio Elaborado por Daniel Perez Proaño
 #include<iostream>
 using namespace std;
 int main (){
 	int a,b,c;
-	cout<<"ingrese el primer numero:"<<endl;
+	cout<<"ingrese el primer numero:"<<endl;//aqui ingresa el usuario su primer numero
 	cin>>a;
-	cout<<"ingrese el segundo numero:"<<endl;
+	cout<<"ingrese el segundo numero:"<<endl;//aqui ingresa el usuario su segundo numero
 	cin>>b;
 	c=a+b;
-	cout<<"el resultado de la suma es:"<<c<<endl;
+	cout<<"el resultado de la suma es:"<<c<<endl;//este es el resultado de la suma
 	
 return 0;
 }
