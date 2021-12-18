8187b0685141f987d72fcbfa4a87cfe198c04630
Daniel Perez Proaño <92699808+Danielp585@users.noreply.github.com>
2021-12-18 13:09:54 -0500

    agregue los comentarios a las lineas de trabajo

--- Actividad C2/Perez Proaño Daniel/comparacion de dos numeros.cpp
+++ Actividad C2/Perez Proaño Daniel/comparacion de dos numeros.cpp
@@ -1,18 +1,19 @@
+//Ejercicio elaborado por Daniel Perez Proaño
 #include<iostream>
 using namespace std;
 int main(){
 	float a,b;
-	cout<<"ingrese el valor de a:"; cin>>a;
-	cout<<"ingrese el valor de b:"; cin>>b;
+	cout<<"ingrese el valor de a:"; cin>>a;//aqui se ingresa el valor que se almacenara en la variable a
+	cout<<"ingrese el valor de b:"; cin>>b;//aqui se ingresa el valor que se almacenara en la variable b
 	
 	if(a==b){
-		cout<<"El valor de a: "<<a<< " es igual que b: "<<b<<endl;		
+		cout<<"El valor de a: "<<a<< " es igual que b: "<<b<<endl;//resultado de la igualdad de las variables		
 		
 	}else{
 		if(a<b){
-			cout<<"a es menor: "<<a<< " que b: "<<b<<endl;
+			cout<<"a es menor: "<<a<< " que b: "<<b<<endl;//resultado variable a es la menor
 		}else{
-			cout<<"b es menor: "<<b<< " que a: "<<a<<endl;
+			cout<<"b es menor: "<<b<< " que a: "<<a<<endl;//resultado la variable b es la menor
 		}
 	}
 	
