62bd2f3c25f5328985fc21ab899dbc39e6ceb875
Eider Elias Mendez Ayovi <92705735+Eidermendez23@users.noreply.github.com>
2021-12-19 12:39:18 -0500

    Add files via upload

---
+++ Actividad C2/Mendez Ayovi Eider/SUMA DE DOS NUMEROS.cpp
@@ -0,0 +1,19 @@
+// hola soy Eider Mendez //
+#include<iostream>
+
+using namespace std; 
+
+int main () {
+	int x,y,z =0 ;
+	
+	cout<< "Digite el primer  numero: "; cin>>x;
+	cout<< "Digite el segundo  numero: "; cin>>y;
+	
+	z = x + y;
+	
+	cout<< "El resultado es: "<<z<<endl; 
+	
+	
+	
+	return 0;
+}
